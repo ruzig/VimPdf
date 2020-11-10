89b590fb649e26a286884693df3ed029e5d8ec9c
phatle <ltanphat@gmail.com>
2020-11-10 15:48:46 +0700

    Open pdf file

--- VimPdf/ViewController.swift
+++ VimPdf/ViewController.swift
@@ -6,21 +6,71 @@
 //
 
 import Cocoa
+import PDFKit
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet weak var commandView: NSTextFieldCell!
+    @IBOutlet weak var pdfView: PDFView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        NSEvent.addLocalMonitorForEvents(matching: .flagsChanged) {
+            self.flagsChanged(with: $0)
+            return $0
+        }
+        NSEvent.addLocalMonitorForEvents(matching: .keyDown) {
+            self.keyDown(with: $0)
+            return $0
+        }
     }
-
+    
+    override func keyDown(with event: NSEvent) {
+        switch event.characters {
+        case "f":
+            print("open file")
+            openFile()
+        default:
+            break
+        }
+        commandView.stringValue = ":" + (event.characters
+                                                ?? "")
+    }
+    
+    func openFile() {
+        let dialog = NSOpenPanel();
+        
+        dialog.title                   = "Choose a file| Our Code World";
+        dialog.showsResizeIndicator    = true;
+        dialog.showsHiddenFiles        = false;
+        dialog.allowsMultipleSelection = false;
+        dialog.canChooseDirectories = false;
+        
+        if (dialog.runModal() ==  NSApplication.ModalResponse.OK) {
+            let result = dialog.url // Pathname of the file
+            
+            if (result != nil) {
+                let path: String = result!.path
+                
+                print(path)
+                self.pdfView.document = PDFDocument(url: result!)
+                
+                // path contains the file path e.g
+                // /Users/ourcodeworld/Desktop/file.txt
+            }
+        } else {
+            // User clicked on "Cancel"
+            return
+        }
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
-
-
 }
 
