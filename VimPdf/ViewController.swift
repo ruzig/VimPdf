5e5813c90625ce42addb7e0fc432c182d283954e
phatle <ltanphat@gmail.com>
2020-11-18 15:40:46 +0700

    Bug fix for empty database

--- VimPdf/ViewController.swift
+++ VimPdf/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: NSViewController {
     var driver: CQueue!
     var marks: [String: Int]!
     var currentDoc: Doc!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,20 +27,24 @@ class ViewController: NSViewController {
         self.driver = CQueue()
         self.marks = ["": 0]
         
+        self.currentDoc = DocModel(context: self.context).last()
         loadLastRead()
         loadMarks()
         self.pdfView.autoScales = true
+        NotificationCenter.default.addObserver(self, selector: #selector(self.saveLastReadPage),name: .PDFViewPageChanged, object: nil)
     }
     func loadMarks() {
-        if (self.currentDoc.marks != nil) {
+        if (self.currentDoc != nil && self.currentDoc.marks != nil) {
             self.marks = try! JSONDecoder().decode([String: Int].self, from: self.currentDoc.marks!)
         }
     }
     
     func saveMark(character: String) {
-        self.marks[character] = (self.pdfView.currentPage?.pageRef!.pageNumber)! - 1
-        self.currentDoc.marks = try! JSONEncoder().encode(self.marks)
-        try! self.context.save()
+        if (self.currentDoc != nil) {
+            self.marks[character] = (self.pdfView.currentPage?.pageRef!.pageNumber)! - 1
+            self.currentDoc.marks = try! JSONEncoder().encode(self.marks)
+            try! self.context.save()
+        }
     }
     
     func loadMark(character: String) {
@@ -67,38 +71,39 @@ class ViewController: NSViewController {
     
     func openFile() {
         self.panel.runModal() { (fileUrl) -> () in
-            let doc = DocModel(context: self.context).create(fileUrl: fileUrl)
-            FilePermission.saveBookmark(doc: doc)
-            self.pdfView.document = PDFDocument(url:fileUrl)
+            self.currentDoc = DocModel(context: self.context).create(fileUrl: fileUrl)
+            FilePermission.saveBookmark(doc: self.currentDoc)
+            loadLastRead()
+            loadMarks()
         }
     }
     
     func loadLastRead() {
-        self.currentDoc = DocModel(context: self.context).last()
-        let url = FilePermission.loadBookmark(doc: self.currentDoc)
-        
-        if url != nil {
-            _ = url!.startAccessingSecurityScopedResource()
-            self.pdfView.document = PDFDocument(url: url!)
+        if (self.currentDoc != nil) {
+            let url = FilePermission.loadBookmark(doc: self.currentDoc)
             
-
-            DispatchQueue.main.async  {
+            if url != nil {
+                _ = url!.startAccessingSecurityScopedResource()
+                
                 let lastReadPage = self.currentDoc!.lastPage
+                self.pdfView.document = PDFDocument(url: url!)
                 if let page = self.pdfView.document?.page(at: Int(lastReadPage)) {
                     self.pdfView.go(to: page)
-                    NotificationCenter.default.addObserver(self, selector: #selector(self.saveLastReadPage),name: .PDFViewPageChanged, object: nil)
                 }
+                
+                url!.stopAccessingSecurityScopedResource()
             }
-            
-            url!.stopAccessingSecurityScopedResource()
         }
     }
     
     @objc private func saveLastReadPage(notification: Notification) {
-        let pdfView = notification.object as! PDFView
-        let page = pdfView.currentDestination?.page?.pageRef?.pageNumber
-        self.currentDoc.lastPage = Int64(page!)
-        try! self.context.save()
+        if self.currentDoc != nil {
+            let pdfView = notification.object as! PDFView
+            let page = pdfView.currentDestination?.page?.pageRef?.pageNumber
+            self.currentDoc.lastPage = Int64(page!)
+            print(page!)
+//            try! self.context.save()
+        }
     }
     
     override func keyDown(with event: NSEvent) {
